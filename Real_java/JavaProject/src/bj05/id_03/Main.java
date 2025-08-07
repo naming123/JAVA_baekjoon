2d61951160861095bb891c2c493470035224749a
naming123 <osm1307@gmail.com>
2025-08-07 16:29:19 +0900

    0807-2

--- Real_java/JavaProject/src/bj05/id_03/Main.java
+++ Real_java/JavaProject/src/bj05/id_03/Main.java
@@ -1,4 +1,4 @@
-package bj05.id_02;
+package bj05.id_03;
 
 import java.io.*;
 
@@ -6,13 +6,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String line;
+        int number = Integer.parseInt(br.readLine());
+        for (int i = 0; i < number; i++) {
+            line =br.readLine();
+            char a = line.charAt(0);
+            char b = line.charAt(line.length()-1);
+            System.out.println(a+""+b);
+        }
+        
 
-        String line = br.readLine();
-
-
-        System.out.println(line.length());
-
-
+        
         br.close();
         bw.flush();
         bw.close();
---
+++ Real_java/JavaProject/src/bj05/id_05/Main.java
@@ -0,0 +1,17 @@
+package bj05.id_04;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char line = br.readLine().charAt(0);
+        int A = line;
+        System.out.println(A); 
+        br.close();
+
+    }
+    
+}
