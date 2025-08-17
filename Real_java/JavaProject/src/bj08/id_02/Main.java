6caad39b7ff15a1128c5204a3dc06191b096840d
naming123 <osm1307@gmail.com>
2025-08-17 22:07:31 +0900

    0817

--- Real_java/JavaProject/src/bj08/id_02/Main.java
+++ Real_java/JavaProject/src/bj08/id_02/Main.java
@@ -1,35 +1,32 @@
-package bj08.id_01;
+package bj08.id_02;
 
 import java.io.*;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] a =br.readLine().split(" ");
-        
-        
-        int num = Integer.parseInt(a[1]);
 
+        int N = Integer.parseInt(a[0]);   
+        int B = Integer.parseInt(a[1]);   
         
-        // 아스키로 접근하는 것이 더 이로울 것이다.
-        // 0~9 : 48~57
-        // A~Z: 65~90 (둘이 붙어 있지 않으므로 if로 따로 비교 필요)
-        
-        int n = a[0].length(); // a[0]의 길이를 구해서 for문의 마지막
-        long sum =0;
-        for (int i=0; i< n; i++){
-            sum = map.get(a[0].charAt(n-i-1))*(long)Math.pow(num, i)+sum; // ^i(XOR)가 제곱이 아니라 pow를 써야한다. (이는 double을 출력하기 때문에 출력형식을 유의해야한다.)
-            // System.out.println(num);
-            // System.out.println(num^i);
-            // System.out.println(a[0]);
-            // System.out.println(a[0].charAt(i)); // 자기가 맵을 char를 썼는지, string을 썼는지 제대로 이해하자.
-            // System.out.println(map.get(a[0].charAt(i)));
+        StringBuilder sb = new StringBuilder();
 
-        }System.out.println(sum);
+        // 몫이 0이 될 때까지 반복
+        while (N > 0) {
+            int R = N % B;  
+            if (R >= 10) {
+                // 10 이상은 알파벳으로 변환
+                sb.append((char) ('A' + (R - 10)));
+            } else {
+                sb.append(R);
+            }
+            N /= B; // 몫으로 갱신
+        }
 
-
+        // 뒤집어서 출력
+        System.out.println(sb.reverse().toString());
 
     }
 }
