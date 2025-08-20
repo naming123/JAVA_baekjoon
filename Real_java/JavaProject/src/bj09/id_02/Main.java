caef2aa5ae10cd125580837c52b3956c9fca07cb
naming123 <osm1307@gmail.com>
2025-08-20 21:43:06 +0900

    0820

---
+++ Real_java/JavaProject/src/bj09/id_02/Main.java
@@ -0,0 +1,20 @@
+package bj09.id_02;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+        return;  // vs Break
+        //
+
+
+        // 리스트로 받아서 인덱스뽑기
+
+
+
+    }
+}
+        
