d66f6af0b082e96ea0e9a88466524fa41b51380a
naming123 <osm1307@gmail.com>
2025-08-23 13:23:13 +0900

    0823

---
+++ Real_java/JavaProject/src/bj11/id_01/Main.java
@@ -0,0 +1,15 @@
+package bj11.id_01;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(1); //  return으로 인해 몇번을 하든 1번이다.
+        System.out.println(0); //  n에 관계없이 상수항 "1"만 가지므로 n의 0차 방정식이다.
+
+    }    
+
+}
+        
