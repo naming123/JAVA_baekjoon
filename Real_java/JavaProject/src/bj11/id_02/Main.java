fdb6b9db85dd81f5e0f18e42ea827673dc1cf8c9
naming123 <osm1307@gmail.com>
2025-08-24 12:41:31 +0900

    0824

--- Real_java/JavaProject/src/bj11/id_02/Main.java
+++ Real_java/JavaProject/src/bj11/id_02/Main.java
@@ -5,9 +5,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println(1); //  return으로 인해 몇번을 하든 1번이다.
-        System.out.println(0); //  n에 관계없이 상수항 "1"만 가지므로 n의 0차 방정식이다.
+        String n = br.readLine();
+        System.out.println(n); //  sum을 하기 위해서는 n개의 모든수에 접근해야하므로 n번이다.
+        System.out.println(1); //  n에 대하여 1차적인 관계를 가진다고 볼 수 있다.
 
     }    
 
