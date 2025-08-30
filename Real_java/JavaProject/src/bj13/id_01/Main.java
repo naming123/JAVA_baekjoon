769c55bad143912a243bfdcf3f745764f89f8113
naming123 <osm1307@gmail.com>
2025-08-30 13:29:53 +0900

    0830

--- Real_java/JavaProject/src/bj13/id_01/Main.java
+++ Real_java/JavaProject/src/bj13/id_01/Main.java
@@ -1,15 +1,26 @@
 package bj13.id_01;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String pp = br.readLine();
-        // set으로 잡아서 pop을 해서
-        // 
+        int num = Integer.parseInt(br.readLine());
+        // array 선언
+        int[] arr = new int[num];
 
+        for (int i=0; i<num;i++){
+            arr[i] =Integer.parseInt(br.readLine());
+
+        }
+        Arrays.sort(arr);
+
+        // 변수 scope가 전역이 아니므로, i를 한번 더 사용해도 됨
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
     }    
 
