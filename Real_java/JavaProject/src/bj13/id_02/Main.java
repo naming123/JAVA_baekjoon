38e30a5c2af2af53de8919008412178d7fafddce
naming123 <osm1307@gmail.com>
2025-08-31 11:54:52 +0900

    0831

---
+++ Real_java/JavaProject/src/bj13/id_02/Main.java
@@ -0,0 +1,26 @@
+package bj13.id_02;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // array 선언
+        int[] arr = new int[5];
+        int sum =0;
+        for (int i=0; i<5; i++){
+            arr[i] =Integer.parseInt(br.readLine());
+            sum = sum +arr[i];
+
+        }
+        Arrays.sort(arr);
+
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+
+    }    
+
+}
+        
