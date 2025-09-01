21b9605ef07abdef73b8ec8c70c5d67497df17ba
naming123 <osm1307@gmail.com>
2025-09-01 10:06:27 +0900

    카운팅정렬

--- Real_java/JavaProject/src/bj13/id_04/Main.java
+++ Real_java/JavaProject/src/bj13/id_04/Main.java
@@ -1,30 +1,40 @@
 package bj13.id_04;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] aa = br.readLine().split("\\s+");
-
-        int N = Integer.parseInt(aa[0]);
-        int k = Integer.parseInt(aa[1]);
+        int N = Integer.parseInt(br.readLine());
 
 
-        String[] score = br.readLine().split("\\s+");
-        
-        int[] arr = new int[N];
-
+        int[] arr = new int[10001];
+        int max =0;
+        int j;
         for (int i=0; i<N; i++){
-            arr[i] =Integer.parseInt(score[i]);
-
+            j =Integer.parseInt(br.readLine());
+            arr[j]++;
+            if (j>max) max = j;
         }
-        Arrays.sort(arr);
-        System.out.println(arr[N-k]);
+        // 주어진 배열 값이 작은 경우 카운팅 정렬을 사용해야함
+        // 배열의 원소 값 자체가 작은 게 아니라, 원소가 가질 수 있는 최대값 범위가 좁다
+        // => 호프만 코딩과 같은 맥락
+        // Arrays.sort(arr);
 
-    }    
+        for (int i=0; i<=max; i++){
+            while (arr[i]>0){
+                bw.write(i+"\n");
+                arr[i]--;
+            }
+        } // 10000000 정도는 되어야 sout이 아닌 bw가 의미가 있음
 
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
 }
         
