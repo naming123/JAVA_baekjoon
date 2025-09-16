00b39eced13cb07685045515869d327d317454f0
naming123 <osm1307@gmail.com>
2025-09-17 00:06:50 +0900

    0917

--- Real_java/JavaProject/src/Main.java
+++ Real_java/JavaProject/src/Main.java
@@ -1,38 +1,35 @@
 // package bj08.id_02;
 
 import java.io.*;
+import java.util.Arrays;
+import java.Math.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] a =br.readLine().split(" ");
-        
-        
-        int num = Integer.parseInt(a[1]);
+        boolean[] isntPrime = new boolean[1000];
 
-        
-        // 아스키로 접근하는 것이 더 이로울 것이다.
-        // 0~9 : 48~57
-        // A~Z: 65~90 (둘이 붙어 있지 않으므로 if로 따로 비교 필요)
-        
-        int n = a[0].length(); // a[0]의 길이를 구해서 for문의 마지막
-        long sum =0;
-        long pow=1;
-        // %를 이용해보자 (나머지구하기)
-        for (int i=0; i< n; i++){
-            // sum = map.get(a[0].charAt(n-i-1))*(long)Math.pow(num, i)+sum; // ^i(XOR)가 제곱이 아니라 pow를 써야한다. (이는 double을 출력하기 때문에 출력형식을 유의해야한다.)
-            // System.out.println(num);
-            // System.out.println(num^i);
-            // System.out.println(a[0]);
-            // System.out.println(a[0].charAt(i)); // 자기가 맵을 char를 썼는지, string을 썼는지 제대로 이해하자.
-            // System.out.println(map.get(a[0].charAt(i)));
+        isntPrime[0] = true;
+        isntPrime[1] = true;
 
-        }System.out.println(sum);
+        for (int p = 2; p * p <= 1000; p++){
+            if ()
+            
+        }
+
+        int cnt=0;
+        for (int i =0; i*i<=1000;i++){
+            if(isntPrime[i]){
+                cnt++;
+            }
+        }System.out.println(cnt);
 
 
 
     }
+    
 }
 
         
---
+++ Real_java/JavaProject/src/bj15/id_03/Main.java
@@ -0,0 +1,33 @@
+package bj15.id_03;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   
+
+        String[] aa = br.readLine().split(" ");
+        int a1 = Integer.parseInt(aa[0]);
+        int a2 = Integer.parseInt(aa[1]);
+        String[] bb = br.readLine().split(" ");
+        int b1 = Integer.parseInt(bb[0]);
+        int b2 = Integer.parseInt(bb[1]);
+
+        int a = a1*b2+a2*b1;
+        int b =a2*b2/GCD(a2,b2);
+
+        System.out.println(a/GCD(a,b)+" "+b/GCD(a,b));
+
+
+
+    }
+    static int GCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
